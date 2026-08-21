aacc4c478a6f773061d13f70e3aa06536fe0d63d
GiteshC <42336770+GiteshC@users.noreply.github.com>
2026-08-21 12:56:50 +0530

    style guide added

--- wp-content/themes/joulestowatts/header.php
+++ wp-content/themes/joulestowatts/header.php
@@ -16,7 +16,7 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
-	 <link href="<?php bloginfo('template_directory'); ?>/css/slick.min.css" rel="stylesheet">
+	<link href="<?php bloginfo('template_directory'); ?>/css/slick.min.css" rel="stylesheet">
     <link href="<?php bloginfo('template_directory'); ?>/css/main.css" rel="stylesheet">
 
 	<?php wp_head(); ?>
@@ -25,37 +25,12 @@
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'joulestowatts' ); ?></a>
+	
+<header>
+	<div class="logoBox">
+		<a href="#">
+			<img src="">
+		</a>
+	</div>
+</header>
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$joulestowatts_description = get_bloginfo( 'description', 'display' );
-			if ( $joulestowatts_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $joulestowatts_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
-
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'joulestowatts' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
